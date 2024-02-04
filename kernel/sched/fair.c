75aa057df2e29ca8ca38b89a32981eb4e3e7f6f7
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-02-04 16:20:29 +0800

    sched: cleanup!
    walt only things arent needed, we going pelt+cass

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -13452,7 +13452,7 @@ static void walt_fixup_nr_big_tasks(struct rq *rq, struct task_struct *p,
 	if (!se)
 		walt_adjust_nr_big_tasks(rq, delta, inc);
 }
-
+#ifdef CONFIG_SCHED_WALT
 /*
  * Check if task is part of a hierarchy where some cfs_rq does not have any
  * runtime left.
@@ -13479,6 +13479,7 @@ static int task_will_be_throttled(struct task_struct *p)
 
 	return 0;
 }
+#endif /* bye walt */
 
 #else /* CONFIG_CFS_BANDWIDTH */
 
@@ -13495,11 +13496,12 @@ static void walt_fixup_nr_big_tasks(struct rq *rq, struct task_struct *p,
 {
 	walt_adjust_nr_big_tasks(rq, delta, inc);
 }
-
+#ifdef CONFIG_SCHED_WALT
 static int task_will_be_throttled(struct task_struct *p)
 {
 	return false;
 }
+#endif /* bye walt */
 
 #endif /* CONFIG_CFS_BANDWIDTH */
 
@@ -13559,6 +13561,7 @@ void walt_rotate_work_init(void)
 	}
 }
 
+#ifdef CONFIG_SCHED_WALT
 #define WALT_ROTATION_THRESHOLD_NS	16000000
 static void walt_check_for_rotation(struct rq *src_rq)
 {
@@ -13694,5 +13697,7 @@ void check_for_migration(struct rq *rq, struct task_struct *p)
 		raw_spin_unlock(&migration_lock);
 	}
 }
+
+#endif /* bye walt */
 
 #endif /* CONFIG_SCHED_WALT */
