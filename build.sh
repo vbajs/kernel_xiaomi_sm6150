36645483722e19454b72a6a5ac037b8b2193f74a
Yahya Wessam <yahyawessam2002@gmail.com>
2024-10-07 01:42:18 +0300

    Scripts: indicate ksu in zip

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ if [[ ! " ${ALLOWED_CODENAMES[@]} " =~ " ${DEVICE} " ]]; then
     exit 1
 fi
 
-ZIPNAME="${DEVICE}-$(date '+%Y%m%d-%H%M').zip"
+ZIPNAME="ksu-${DEVICE}-$(date '+%Y%m%d-%H%M').zip"
 
 export ARCH=arm64
 export KBUILD_BUILD_USER=vbajs
