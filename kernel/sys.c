9724a5b32cce1947eca6e4816db8b23a4082d0fe
Yahya Wessam <yahyawessam2002@gmail.com>
2024-10-15 12:15:30 +0300

    syscall: Move SUSFS_SPOOF_UNAME to newuname
    Potiential bug: conflict with ebpf uname spoof

--- kernel/sys.c
+++ kernel/sys.c
@@ -1194,7 +1194,14 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+	if (likely(!susfs_spoof_uname(&tmp)))
+		goto bypass_orig_flow;
+#endif
 	memcpy(&tmp, utsname(), sizeof(tmp));
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+bypass_orig_flow:
+#endif
 	if (!is_legacy_ebpf) {
 	  if (!strncmp(current->comm, "netbpfload", 10) &&
 	    current->pid != netbpfload_pid) {
@@ -1227,14 +1234,7 @@ SYSCALL_DEFINE1(uname, struct old_utsname __user *, name)
 		return -EFAULT;
 
 	down_read(&uts_sem);
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-	if (likely(!susfs_spoof_uname(&tmp)))
-		goto bypass_orig_flow;
-#endif
 	memcpy(&tmp, utsname(), sizeof(tmp));
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-bypass_orig_flow:
-#endif
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
