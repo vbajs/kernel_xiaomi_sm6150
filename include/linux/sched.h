376ad99a0de9a46a7113556e952b962b1dd1167f
Yahya Wessam <yahyawessam2002@gmail.com>
2024-10-21 15:00:10 +0300

    KSU 1.0.1 + SUSFS 1.4.2
    SUSFS patches now handle path_ummount so let it handle that
    Before compiling, please rename the "git" directory to ".git"

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1462,6 +1462,10 @@ struct task_struct {
 		bool free_stack;
 	} async_free;
 
+#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
+	u64 android_kabi_reserved1;
+	u64 android_kabi_reserved2;
+#endif
 	/*
 	 * New fields for task_struct should be added above here, so that
 	 * they are included in the randomized portion of task_struct.
